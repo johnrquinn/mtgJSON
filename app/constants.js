646d6b83db078308e1737eeb9abe15e66f3d4349
Shawn Savageau <shawn.savageau@gmail.com>
2022-12-12 20:39:51 -0700

    split json files && merge in dg data

---
+++ app/constants.js
@@ -0,0 +1,46 @@
+
+const typesToRemove = [
+  'plane',
+  'scheme',
+  'vanguard',
+  'token',
+  'token creature',
+  'emblem',
+  'card // card',
+  'phenomenon',
+  'card',
+  'legendary enchantment — background',
+  'basic land',
+  'token artifact creature',
+  'token legendary creature',
+];
+const setsToRemove = [
+  'memorabilia',
+  'funny',
+  'token',
+];
+const outputSections = [
+  [
+    'creature',
+    'artifact creature',
+    'enchantment creature',
+    'legendary creature',
+  ],
+  [
+    'enchantment',
+    'enchantment aura',
+  ],
+  [
+    'sorcery',
+    'instant',
+  ],
+  ['artifact'],
+  ['land'],
+  ['planeswalker'],
+];
+
+export {
+  typesToRemove,
+  setsToRemove,
+  outputSections,
+};
